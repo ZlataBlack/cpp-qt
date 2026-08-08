e6702c1d5e0ebae68829e1862297b5847cbab700
ZlataBlack <zlata109vika@gmail.com>
2026-08-08 22:20:14 +0300

    Update mainwindow.h

--- problems/sprint2/final-task/submission/mainwindow.h
+++ problems/sprint2/final-task/submission/mainwindow.h
@@ -1,8 +1,8 @@
 #pragma once
 
-#include "calculator.h"
-
 #include <QMainWindow>
+
+#include "calculator.h"
 
 QT_BEGIN_NAMESPACE
 namespace Ui {
@@ -10,15 +10,54 @@ class MainWindow;
 }
 QT_END_NAMESPACE
 
-class MainWindow : public QMainWindow {
+enum class Operation {
+    NO_OPERATION,
+    ADDITION,
+    SUBTRACTION,
+    MULTIPLICATION,
+    DIVISION,
+    POWER
+};
+
+class MainWindow : public QMainWindow
+{
     Q_OBJECT
 
 public:
-    MainWindow(QWidget* parent = nullptr);
+    explicit MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
 
 private slots:
+    void OnDigitButtonClicked();
+    void OnDotButtonClicked();
+    void OnSignButtonClicked();
+    void OnBackspaceButtonClicked();
+
+    void OnPowerButtonClicked();
+    void OnDivideButtonClicked();
+    void OnMultiplyButtonClicked();
+    void OnSubtractButtonClicked();
+    void OnAddButtonClicked();
+
+    void OnEqualsButtonClicked();
+    void OnClearButtonClicked();
+
+    void OnMemorySaveButtonClicked();
+    void OnMemoryClearButtonClicked();
+    void OnMemoryLoadButtonClicked();
 
 private:
-    Ui::MainWindow* ui;
+    void SetText(const QString& text);
+    void AddText(const QString& suffix);
+    void SetOperation(Operation operation);
+
+    Ui::MainWindow *ui_;
+
+    Calculator calculator_;
+    QString input_number_;
+    Number active_number_ = 0;
+    Operation current_operation_ = Operation::NO_OPERATION;
+
+    Number memory_value_ = 0;
+    bool has_memory_ = false;
 };
