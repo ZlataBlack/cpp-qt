6c699eb09453883c64996d7008616d5dcf9bf27e
ZlataBlack <zlata109vika@gmail.com>
2026-08-08 22:12:58 +0300

    Update calculator.cpp

--- problems/sprint2/final-task/submission/calculator.cpp
+++ problems/sprint2/final-task/submission/calculator.cpp
@@ -1,1 +1,29 @@
-// Ваше решение задачи о калькуляторе с классом.
+#include "calculator.h"
+
+void Calculator::Set(Number number) {
+    number_ = number;
+}
+
+Number Calculator::GetNumber() const {
+    return number_;
+}
+
+void Calculator::Add(Number addend) {
+    number_ += addend;
+}
+
+void Calculator::Sub(Number subtrahend) {
+    number_ -= subtrahend;
+}
+
+void Calculator::Mul(Number multiplier) {
+    number_ *= multiplier;
+}
+
+void Calculator::Div(Number divisor) {
+    number_ /= divisor;
+}
+
+void Calculator::Pow(Number exponent) {
+    number_ = std::pow(number_, exponent);
+}
