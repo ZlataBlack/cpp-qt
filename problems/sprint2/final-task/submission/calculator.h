fcb868deabc8fe50b647fcfc5ebb9c2547c6b404
ZlataBlack <zlata109vika@gmail.com>
2026-08-08 22:13:36 +0300

    Update calculator.h

--- problems/sprint2/final-task/submission/calculator.h
+++ problems/sprint2/final-task/submission/calculator.h
@@ -1,1 +1,20 @@
-// Ваше решение задачи о калькуляторе с классом.
+#pragma once
+
+#include <cmath>
+
+using Number = double;
+
+class Calculator {
+public:
+    void Set(Number number);
+    Number GetNumber() const;
+
+    void Add(Number addend);
+    void Sub(Number subtrahend);
+    void Mul(Number multiplier);
+    void Div(Number divisor);
+    void Pow(Number exponent);
+
+private:
+    Number number_ = 0;
+};
